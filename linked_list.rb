13d0490d1305f402b937bd022bfb27c7fb95551d
Sid Ngeth <sngeth@gmail.com>
2017-02-14 12:32:54 -0500

    Add tests for linked list

--- linked_list.rb
+++ linked_list.rb
@@ -1,7 +1,7 @@
 class Node
   attr_accessor :data, :next_node
 
-  def initialize(data, next_node)
+  def initialize(data, next_node = nil)
     @data = data
     @next_node = next_node
   end
@@ -11,15 +11,19 @@ class LinkedList
   attr_accessor :head
 
   def add_first(item)
-    head = Node.new(item, head)
+    @head = Node.new(item, head)
   end
 
   def traverse
     temp_node = head
+    arr = []
 
     while temp_node != nil do
+      arr << temp_node.data
       temp_node = temp_node.next_node
     end
+
+    arr
   end
 
   def add_last(item)
@@ -28,7 +32,7 @@ class LinkedList
     else
       temp_node = head
 
-      while(temp_node != nil) do
+      while(temp_node.next_node != nil) do
         temp_node = temp_node.next_node
       end
 
@@ -36,18 +40,18 @@ class LinkedList
     end
   end
 
-  def insert_after(key, node_to_insert)
+  def insert_after(key, value)
     temp_node = head
 
     while(temp_node != nil && !(temp_node.data == key)) do
       temp_node = temp_node.next
     end
 
-    temp_node.next_node = Node.new(node_to_insert, temp_node.next_node) if(temp_node != nil)
+    temp_node.next_node = Node.new(value, temp_node.next_node) if temp_node != nil
   end
 
   def insert_before(key, node_to_insert)
-    return nil if(head == nil)
+    return nil if head == nil
 
     if(head.data == key)
       return add_first(node_to_insert)
@@ -61,13 +65,16 @@ class LinkedList
       cur = cur.next_node
     end
 
-    prev.next_node = Node.new(node_to_insert, cur) if(cur != nil)
+    prev.next_node = Node.new(node_to_insert, cur) if cur != nil
   end
 
   def remove(key)
-    raise "Cannot delete" if head == nil
+    raise "Cannot delete" if @head == nil
 
-    head = head.next_node if (head.data == key)
+    if(@head.data == key)
+      @head = head.next_node
+      return
+    end
 
     cur = head
     prev = nil
