20d6087928bbc531b2c8088c6a2f7ffaf3e85596
Sid Ngeth <sngeth@gmail.com>
2017-07-30 14:05:06 -0400

    Remove duplicates from linked list

--- reverse_linked_list.rb
+++ reverse_linked_list.rb
@@ -1,4 +1,6 @@
 require "minitest/autorun"
+require "set"
+require "byebug"
 
 Node = Struct.new(:data, :next)
 
@@ -7,6 +9,18 @@ class LinkedList
 
   def initialize(head)
     @head = head
+  end
+
+  def to_s
+    curr = head
+    ll = []
+
+    while curr
+      ll << curr.data
+      curr = curr.next
+    end
+
+    ll.to_s
   end
 
   def reverse_iterative(head)
@@ -44,6 +58,25 @@ class LinkedList
     head.next = nil
     @head = reversed_head
   end
+
+  def remove_dups
+    return head if !head
+
+    dups = Set.new
+    dups.add(head.data)
+    curr = head
+
+    while curr.next
+      if dups.include?(curr.next.data)
+        curr.next = curr.next.next
+      else
+        dups.add(curr.next.data)
+        curr = curr.next
+      end
+    end
+
+    head
+  end
 end
 
 describe LinkedList do
@@ -72,6 +105,22 @@ describe LinkedList do
       @linked_list.head.next.data.must_equal 21
       @linked_list.head.next.next.data.must_equal 14
       @linked_list.head.next.next.next.data.must_equal 7
+    end
+  end
+
+  describe "#remove_dups" do
+    it "head reference to list with no duplicates" do
+      g = Node.new(4, nil)
+      f = Node.new(11, g)
+      e = Node.new(7, f)
+      d = Node.new(9, e)
+      c = Node.new(4, d)
+      b = Node.new(7, c)
+      head = Node.new(4, b)
+
+      linked_list = LinkedList.new(head)
+      linked_list.remove_dups
+      linked_list.to_s.must_equal "[4, 7, 9, 11]"
     end
   end
 end
